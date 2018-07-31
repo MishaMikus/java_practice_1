7b1a4deddca6d43576964b63ce43085e75502684
Yevhen Bilevych <ybilevych@gmail.com>
2018-07-31 17:51:16 +0300

    Update CollectionIteration.java
    Added task decription

--- src/CollectionIteration.java
+++ src/CollectionIteration.java
@@ -1,3 +1,18 @@
+/*
+Task description
+ Create class Student that has following fields: name, sex, group, language, year.
+ Student can belong to one of three-four groups, know one to five languages, study 1-4 year. Names are mostly unique. Sex is an enum
+ Constructor should accept all 5 attributes above and set fields.
+ Getters can return values of fields
+ 
+ 1. Create the list of students, at leat 10 in total. The know few langauges, belong to one of three-four groups, study 1-4 year
+ 2. Create and return a unique set of languages students know
+ 3. Create a list (and output it) of all male students;
+ 3. Calculate the dispertion of students by study years
+ 4. Build a map: what sudents belong to which group
+ 5. Create a set of groups (unique items), students belong to
+*/
+
 public class CollectionIteration {
 
 }
